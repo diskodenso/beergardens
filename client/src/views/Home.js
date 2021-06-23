f7abd3c9ea0fe432013b1b0c2d627a038d9456cd
Denis Olf <denisadnanolf@gmail.com>
2021-06-23 14:00:12 +0200

    fetching api from own backend

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,36 +1,43 @@
 import React, { useEffect, useState, useContext, createContext } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Loading from "../components/Loading.js";
 
-function Home() {
-    const [beergarden, setBeergarden] = useState([])
- const { beergardens } = useContext(BeergardensContext)
+    function Home() {
+        const [beergarden, setBeergarden] = useState([]);
+        const [loading, setLoading] = useState(true);
+ // const { beergardens } = useContext(BeergardensContext)
     const myBeergarden = async () => {
         try {
             const response = await fetch(`http://localhost:5000/beergardens/all`);
         const data = await response.json();
-        setBeergarden(data);
+            setBeergarden(data);
+            setLoading(false);
         console.log(response);
         console.log(data);
         } catch(err) {
             console.log(err)
-        }
-        
+        }    
     }
 
     useEffect(() => {
         myBeergarden();
     }, []);
-   return  />;
-
+        return (loading ? <Loading></Loading>:
+            beergarden.map((oneBeergarden, index) => {
+                return (
+                    <div key={oneBeergarden._id}>
+                    <img src={oneBeergarden.picture}></img>
+                        <p>{oneBeergarden.name }</p>
+                        </div>
+                )
             })
-        ) : (
-            <h2>loading..</h2>
-        )}
-    </div>
-        </div>
-);
-        }
+        )
+}
 export default Home;
 
-// use map function - takes each opject in an array and returns it
-                    // if loading false show description if loading is true - loading
+
+// // use map function - takes each opject in an array and returns it
+
+// create a beergarden component
+// pass it from home component to beergarden component
+// style it (material ui)
