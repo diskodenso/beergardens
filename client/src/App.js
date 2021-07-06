60d347200a786bb60d1fccf500217c1a0d5ee276
Denis Olf <denisadnanolf@gmail.com>
2021-07-06 09:26:34 +0200

    registration and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
 import React, { useContext } from 'react';
 import Home from "./views/Home.js";
+import Maps from "./views/Maps.js";
 import About from "./views/About.js";
 import Favorites from "./views/Favorites.js";
 import { AuthContext } from "./context/AuthContext.js";
-
+import NavBar from "./components/NavBar.js";
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom";
+import SignIn from './components/SignIn.js';
+import SignUp from './components/SignUp.js';
+ 
 // do the react router 
 function App() {
-  const { user, setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  // const { user, setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
   return (
-    <Home></Home>
+      <Router>
+            <div className="App">
+
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path="/signIn" component={SignIn}>
+          </Route>
+          <Route exact path="/signUp" component={SignUp}>
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites/>
+          </Route>
+        </Switch>
+            </div>
+      </Router>
   );
 }
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,140 +1,69 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState, useRef, useEffect } from 'react';
+import AuthService from "../services/AuthService.js";
+import Message from "../components/Message.js";
 
-function Copyright() {
+
+const SignUp = props => {
+  const [user, setUser] = useState({ userName: "", password: "" });
+  const [message, setMessage] = useState(null);
+  let timerID = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timerID);
+    }
+  }, []);
+  
+  // create onChange Functions by getting the event object (e)
+  // then we prevent the default action from accuring
+  // we updating the setUser to whats the user is typing in
+  const onChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+  const resetForm = () => {
+    setUser({ userName: "", password: "" });
+  }
+  // create onSubmit function
+  // get the event object and prevent the default from having
+  const onSubmit = e => {
+    e.preventDefault();
+    // use Authservice we have coded and we pass in the user which
+    // returns a promise (so we say .then) and we should get back the
+    // parsed data
+    AuthService.register(user).then(data => {
+      const { message } = data;
+      setMessage(message);
+      resetForm();
+      if (!message.msgError) {
+        timerID = setTimeout(() => {
+          props.history.push("/signIp")
+        }, 2000)
+      }
+    });
+  }
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    <div>
+      <form onSubmit={onSubmit}>
+        <h3>Sign Up!</h3>
+        <label htmlFor="userName" className="sr-only">Username</label>
+        <input type="text" name="userName" onChange={onChange}
+        className="form control"
+          placeholder="Enter Username" />
+        <label htmlFor="email" className="sr-only">Email</label>
+        <input type="email" name="email" onChange={onChange}
+        className="form control"
+          placeholder="Enter valid Email" />
+         <label htmlFor="password" className="sr-only">Password</label>
+        <input type="password" name="password" onChange={onChange}
+        className="form control"
+        placeholder="Enter Password"/>
+<button className="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
+      </form>
+      {/* underneath our form we have our message component incase we
+      have a message to display - if message is not null we render
+    the message component ":" otherwise we dont want to render anything */}
+      {message ? <Message message={message}/> : null}
+    </div>
+  )
 }
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function SignUp() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+export default SignUp;
